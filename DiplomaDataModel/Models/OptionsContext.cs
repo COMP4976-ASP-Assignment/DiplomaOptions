55c44849fa6a4235c77774ed9e571aa1d1ffc071
Adam Slater <Adam Slater>
2016-02-13 20:49:49 -0800

    Added db context

---
+++ DiplomaDataModel/Models/OptionsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomaDataModel.Models
+{
+    public class OptionsContext : DbContext
+    {
+        public OptionsContext() : base("DefaultConnection") { }
+
+        public DbSet<Option>   Options { get; set; }
+        public DbSet<Choice>   Choices { get; set; }
+        public DbSet<YearTerm> YearTerms { get; set; }
+    }
+}
