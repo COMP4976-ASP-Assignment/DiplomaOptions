583f9c1d8f209bdfc915eea4a96a16cae7395ef3
joxtoyod <joxched@gmail.com>
2016-02-17 08:42:32 +0000

    updated choices controller, check duplicates in choices

--- OptionsWebSite/Controllers/ChoicesController.cs
+++ OptionsWebSite/Controllers/ChoicesController.cs
@@ -96,19 +96,41 @@ namespace OptionsWebSite.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "ChoiceId,StudentId,YearTermId,StudentFirstName,StudentLastName,FirstChoiceOptionId,SecondChoiceOptionId,ThirdChoiceOptionId,FourthChoiceOptionId")] Choice choice)
         {
+            int[] userChoices = {(int) choice.FirstChoiceOptionId, (int) choice.SecondChoiceOptionId, (int)choice.ThirdChoiceOptionId, (int)choice.FourthChoiceOptionId };
+            IList<YearTerm> yearTerms = db.YearTerms.ToList();
+            var term = from query in yearTerms
+                       where query.IsDefault.Equals(true)
+                       select query;
+            var name = new Dictionary<int, string>();
+
+            name[10] = "Winter";
+            name[20] = "Spring/Summer";
+            name[30] = "Fall";
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+
+
+            if (userChoices.Distinct().Count() != userChoices.Count())
+            {
+                ViewBag.YearTermIdValue = term.ToArray().ElementAt(0).YearTermId;
+                ViewBag.YearTermDisplay = term.ToArray().ElementAt(0).Year + " " + name[term.ToArray().ElementAt(0).Term];
+                ViewBag.FirstChoiceOptionId = new SelectList(db.Options, "OptionId", "Title", choice.FirstChoiceOptionId);
+                ViewBag.FourthChoiceOptionId = new SelectList(db.Options, "OptionId", "Title", choice.FourthChoiceOptionId);
+                ViewBag.SecondChoiceOptionId = new SelectList(db.Options, "OptionId", "Title", choice.SecondChoiceOptionId);
+                ViewBag.ThirdChoiceOptionId = new SelectList(db.Options, "OptionId", "Title", choice.ThirdChoiceOptionId);
+                ViewBag.ErrorChoices = "Choices cannot be the same";
+                ViewBag.StudentId = user.UserName;
+
+
+                return View(choice);
+            }
+               
             var sId = db.Choices.ToList();
             foreach (var id in sId)
             {
                 if (id.StudentId == choice.StudentId)
                 {
                     ViewBag.Error = "Already Choosen options";
-                    ViewBag.FirstChoiceOptionId = new SelectList(db.Options, "OptionId", "Title", choice.FirstChoiceOptionId);
-                    ViewBag.FourthChoiceOptionId = new SelectList(db.Options, "OptionId", "Title", choice.FourthChoiceOptionId);
-                    ViewBag.SecondChoiceOptionId = new SelectList(db.Options, "OptionId", "Title", choice.SecondChoiceOptionId);
-                    ViewBag.ThirdChoiceOptionId = new SelectList(db.Options, "OptionId", "Title", choice.ThirdChoiceOptionId);
-                    ViewBag.YearTermId = new SelectList(db.YearTerms, "YearTermId", "YearTermId", choice.YearTermId);
-
-                    return View(choice);
+                    return View();
                 }
             }
 
@@ -123,8 +145,11 @@ namespace OptionsWebSite.Controllers
             ViewBag.FourthChoiceOptionId = new SelectList(db.Options, "OptionId", "Title", choice.FourthChoiceOptionId);
             ViewBag.SecondChoiceOptionId = new SelectList(db.Options, "OptionId", "Title", choice.SecondChoiceOptionId);
             ViewBag.ThirdChoiceOptionId = new SelectList(db.Options, "OptionId", "Title", choice.ThirdChoiceOptionId);
-            ViewBag.YearTermId = new SelectList(db.YearTerms, "YearTermId", "YearTermId", choice.YearTermId);
-      
+            ViewBag.YearTermIdValue = term.ToArray().ElementAt(0).YearTermId;
+            ViewBag.YearTermDisplay = term.ToArray().ElementAt(0).Year + " " + name[term.ToArray().ElementAt(0).Term];
+            ViewBag.StudentId = user.UserName;
+
+
             return View(choice);
         }
 
